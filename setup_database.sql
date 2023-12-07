df48bf61714d43d4b3658cf6f8f12707914541e4
hihihi773 <123101247+hihihi773@users.noreply.github.com>
2023-12-06 23:44:16 -0800

    setup_database.sql

--- setup_database.sql
+++ setup_database.sql
@@ -1,27 +1,10 @@
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'PHP_project')
-BEGIN
-    CREATE DATABASE [PHP_project];
-END
-GO
+CREATE DATABASE IF NOT EXISTS `PHP_project`;
 
+USE `PHP_project`;
 
-USE [PHP_project];
-GO
-
--- Check if the users table exists and create if it does not
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = N'users' AND type = 'U')
-BEGIN
-    CREATE TABLE [users] (
-        [id] INT IDENTITY(1,1) PRIMARY KEY,
-        [full_name] NVARCHAR(255) NOT NULL,
-        [email] NVARCHAR(255) NOT NULL,
-        [password] NVARCHAR(255) NOT NULL
-    );
-END
-GO
-
--- Insert initial data
-INSERT INTO [users] ([full_name], [email], [password]) VALUES
-(N'himanshu', 'dhimasnhu238@gmail.com', '1234qwer'),
-(N'Jane Smith', 'jane.smith@example.com', 'password456');
-GO
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `full_name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL
+);
